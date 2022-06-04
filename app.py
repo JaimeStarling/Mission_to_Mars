a915dd9b51bfd170daf262db530cdaae663479ad
Jaime Starling <101942166+JaimeStarling@users.noreply.github.com>
2022-06-04 16:22:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # 10.5.1
 
-# from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
 
